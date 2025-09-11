2189dedfb3f0bd6317f65ed6c98ce55cc8a84023
Kotebus666 <kotebus666@gmail.com>
2025-09-11 15:56:47 +0300

    Exercise 2: Prep and Polish Fix text truncation

--- src/components/SecondaryStory/SecondaryStory.jsx
+++ src/components/SecondaryStory/SecondaryStory.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const SecondaryStory = ({ id, title, image, location, abstract }) => {
-  return (
-    <a href={`/story/${id}`}>
-      <Wrapper>
-        <Image alt={image.alt} src={image.src} />
-        <Heading>{title}</Heading>
-        <Abstract>{abstract}</Abstract>
-      </Wrapper>
-    </a>
-  );
+    return (
+        <a href={`/story/${id}`}>
+            <Wrapper>
+                <Image alt={image.alt} src={image.src}/>
+                <Heading>{title}</Heading>
+                <AbstractWrapper>
+                    <Abstract>{abstract}</Abstract>
+                </AbstractWrapper>
+            </Wrapper>
+        </a>
+    );
 };
 
 const Wrapper = styled.article`
@@ -41,8 +43,11 @@ const Heading = styled.h2`
   margin-top: -2px;
 `;
 
+const AbstractWrapper = styled.div`
+    grid-area: abstract;
+`;
+
 const Abstract = styled.p`
-  grid-area: abstract;
   font-size: 1rem;
   white-space: pre-wrap;
 
